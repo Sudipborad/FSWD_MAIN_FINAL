ed98f5094218a41d49f2a3262c23a7b69e9c7871
Sudipborad <105632642+Sudipborad@users.noreply.github.com>
2025-05-05 12:17:34 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,8 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Navigate to="employee" />} />
-          {/* <Route path='/register' element={<Signup/>}/>
-          <Route path='/login' element={<Login/>}/> */}
+          <Route path='/register' element={<Signup/>}/>
+          <Route path='/login' element={<Login/>}/> 
           <Route path="/employee" element={<EmployeeManagementApp />} />
           <Route path="/employee/:id" element={<EmployeeDetails />} />
         </Routes>
